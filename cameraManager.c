8725cf593c5996cd2d084b9d102b530a59b11665
Pupye <fullhouseya89@gmail.com>
2020-03-10 17:10:22 +0600

    processes can be added and deleted

--- cameraManager.c
+++ cameraManager.c
@@ -12,7 +12,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <stdbool.h>
 #include "dataBlock.h"
 
 #define BUFSIZE 4096
@@ -76,12 +76,22 @@ void initSeparateCamera(struct Camera *camera)
     sem_init(&camera->servicedLock, 2, 1);
     camera->serviced = 0;
     sem_init(&camera->onOffLock, 2, 1);
-    camera->on = 1;
+    camera->on = 0;
+}
+
+void activateCameras(struct DataBlock *dataBlock, int numberOfCameras)
+{
+    dataBlock->numberOfActiveCameras = numberOfCameras;
+    for (size_t i = 0; i < numberOfCameras; i++)
+    {
+        dataBlock->cameras[i].on = 1;
+    }
 }
 
 void initCameras(struct DataBlock *ShmDataBlock, int numberOfCameras)
 {
-    ShmDataBlock->numberOfActiveCameras = numberOfCameras;
+
+    sem_init(&ShmDataBlock->commandStatus, 2, 0);
     for (size_t i = 0; i < numberOfCameras; i++)
     {
         initSeparateCamera(&ShmDataBlock->cameras[i]);
@@ -109,13 +119,13 @@ int main(int argc, char *argv[])
     key_t ShmKEY;
     struct DataBlock *ShmDataBlock;
     /*to get unigue identifier*/
-    ShmKEY = ftok("./hello", 'x');
+    ShmKEY = ftok(".", 23);
     //allocating shared block
     ShmDataBlock = (struct DataBlock *)initSharedMemory(ShmKEY);
     //dataBlock initialization
     initCamData(ShmDataBlock);
-    initCameras(ShmDataBlock, numberOfCameras);
-
+    initCameras(ShmDataBlock, 10);                  //cameraSockets
+    activateCameras(ShmDataBlock, numberOfCameras); //4 active cameras
     for (size_t i = 0; i < 10; i++)
     {
         int camID = ShmDataBlock->cameras[i].cameraId;
@@ -138,14 +148,51 @@ int main(int argc, char *argv[])
             int commandType = getCommand(buf);
             if (commandType == ADD)
             {
-                // how we are going to add new camera??
+                struct Camera *camera;
                 // look for off cameraSockets
                 // and on it (initialize with new variable)
-                // then increment the number of active cameras.
+                // then increment the number of active cameras in order to limit
+                // after that you should post commandStatus and set the command into ADD
+                for (size_t i = 0; i < 10; i++)
+                {
+                    camera = &ShmDataBlock->cameras[i];
+
+                    sem_wait(&camera->onOffLock);
+                    if (!(camera->on))
+                    {
+                        camera->on = 1;
+                        camera->serviced = 0;
+                        ShmDataBlock->commandType = ADD;
+                        sem_post(&ShmDataBlock->commandStatus); //this status should not be 2
+                        sem_post(&camera->onOffLock);
+                        break;
+                    }
+                    sem_post(&camera->onOffLock);
+                }
+
                 printf("You've entered add\n");
             }
             else if (commandType == REMOVE)
             {
+                struct Camera *camera;
+                for (size_t i = 0; i < 10; i++)
+                {
+                    camera = &ShmDataBlock->cameras[i];
+                    sem_wait(&camera->onOffLock);
+                    if (camera->on)
+                    {
+                        camera->on = 0;
+
+                        ShmDataBlock->commandType = REMOVE;
+                        sem_post(&ShmDataBlock->commandStatus);
+                        sem_post(&camera->onOffLock);
+                        break;
+                    }
+                    sem_post(&camera->onOffLock);
+                }
+
+                sem_wait(&camera->gracefullFinishLock);
+                printf("The camera with ID: %i was removed\n", camera->cameraId);
                 //first we want to find the camera that is on and off it if it serviced wait for gracefull
                 //shutdown
                 //first we wait for the on off lock
@@ -154,14 +201,17 @@ int main(int argc, char *argv[])
                 //the remove completed
                 //do we need to decrease the number of active cameras??
                 //or process should be responsible to this?? I guess process
-                sem_wait(&ShmDataBlock->cameras[0].onOffLock);
-                ShmDataBlock->cameras[0].on = 0; //this means that we've offed the camera
-                //consider this as a signal
-                sem_post(&ShmDataBlock->cameras[0].onOffLock);
-                //do we need gracefullshutdown? Yes we can not add unless we are sure that it is shutdown
-                sem_wait(&ShmDataBlock->cameras[0].gracefullFinishLock);
-                printf("The camera with ID: %i was removed\n", ShmDataBlock->cameras[0].cameraId);
-                //here it is off and can be replaced with other process.
+                // sem_wait(&ShmDataBlock->cameras[0].onOffLock);
+                // ShmDataBlock->cameras[0].on = 0; //this means that we've offed the camera
+                // //consider this as a signal
+                // ShmDataBlock->commandType = REMOVE;
+                // sem_post(&ShmDataBlock->commandStatus);
+
+                // sem_post(&ShmDataBlock->cameras[0].onOffLock);
+                // //do we need gracefullshutdown? Yes we can not add unless we are sure that it is shutdown
+                // sem_wait(&ShmDataBlock->cameras[0].gracefullFinishLock);
+                // printf("The camera with ID: %i was removed\n", ShmDataBlock->cameras[0].cameraId);
+                // //here it is off and can be replaced with other process.
                 //how we can check it?
             }
             else if (commandType == STOP)
@@ -176,36 +226,83 @@ int main(int argc, char *argv[])
     }
     else
     {
-        struct Camera *chosenCamera;
-        int fd;
-        fd = open("logs.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-        for (size_t i = 0; i < 10; i++)
+        int status;
+        //initblock
+        for (size_t i = 0; i < ShmDataBlock->numberOfActiveCameras; i++)
         {
-            chosenCamera = &ShmDataBlock->cameras[i];
-            sem_wait(&chosenCamera->onOffLock);
-            if (chosenCamera->on && !(chosenCamera->serviced))
+            pid = fork();
+
+            if (pid == 0)
             {
                 break;
             }
-        }
-        chosenCamera->serviced = 1;
-        sem_post(&chosenCamera->onOffLock);
-        //camera is chosen for service
-        for (;;)
-        {
-            if (!(chosenCamera->on))
-            {
-                break;
-            }
-            dprintf(fd, "service is on for %i\n", chosenCamera->cameraId);
-            fflush(stdout);
-            sleep(5);
         }
 
-        ShmDataBlock->numberOfActiveCameras = ShmDataBlock->numberOfActiveCameras - 1;
-        printf("The process is shut down\n");
-        fflush(stdout);
-        sem_post(&chosenCamera->gracefullFinishLock);
-        exit(1);
+        //initblock
+        //controllblock
+        if (pid > 0)
+        {
+            for (;;)
+            {
+                sem_wait(&ShmDataBlock->commandStatus);
+                if (ShmDataBlock->commandType == REMOVE)
+                {
+                    wait(&status);
+                }
+                else if (ShmDataBlock->commandType == ADD)
+                {
+                    pid = fork();
+                }
+                else
+                {
+                    break; //in this case we will wait out all childs to finish and exit
+                }
+                if (pid == 0)
+                {
+                    break;
+                }
+            }
+        }
+        //controllblock
+
+        //do somethingblock
+        if (pid == 0)
+        {
+            int fd;
+            fd = open("logs.txt", O_CREAT | O_APPEND | O_WRONLY, 0644);
+            struct Camera *chosenCamera;
+            for (size_t i = 0; i < 10; i++)
+            {
+                chosenCamera = &ShmDataBlock->cameras[i];
+                sem_wait(&chosenCamera->onOffLock);
+                if (chosenCamera->on && !(chosenCamera->serviced)) //problem is here
+                {
+                    break;
+                }
+                sem_post(&chosenCamera->onOffLock);
+            }
+            chosenCamera->serviced = 1;
+            sem_post(&chosenCamera->onOffLock);
+            //camera is chosen for service
+            for (;;)
+            {
+                sem_wait(&chosenCamera->onOffLock);
+                if (!(chosenCamera->on))
+                {
+                    sem_post(&chosenCamera->onOffLock);
+                    break;
+                }
+                sem_post(&chosenCamera->onOffLock);
+                dprintf(fd, "service is on for %i\n", chosenCamera->cameraId);
+                fflush(stdout);
+                sleep(2);
+            }
+            //I think we need a lock here
+            ShmDataBlock->numberOfActiveCameras = ShmDataBlock->numberOfActiveCameras - 1;
+            printf("The process is shut down\n");
+            fflush(stdout);
+            sem_post(&chosenCamera->gracefullFinishLock);
+            exit(1);
+        }
     }
 }
