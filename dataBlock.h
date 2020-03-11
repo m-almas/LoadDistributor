047df72744780e095af770a03fb69a466832724e
Pupye <fullhouseya89@gmail.com>
2020-03-12 00:22:18 +0600

    introducing maximum camera numbers

--- dataBlock.h
+++ dataBlock.h
@@ -7,6 +7,7 @@
 #define ON 2
 #define OFF 3
 #define BUSY 5
+#define MAX_CAM_NUMBER 10
 //****
 
 struct CameraSocket
@@ -33,23 +34,25 @@ struct CamData
 
 struct DataBlock
 {
-    struct CameraSocket cameraSockets[10]; // the cameras will connect to these sockets
-    int numberOfActiveCameras;             // should resemble number active processes
-                                           // guess this should also have lock
-    sem_t commandIndicator;                // initial value is 0. it's purpose is
-                                           // to indicate about new command
-    int commandType;                       // indicated commandType
+    struct CameraSocket
+        cameraSockets[MAX_CAM_NUMBER]; // the cameras will connect to these sockets
+    int numberOfActiveCameras;         // should resemble number active processes
+                                       // guess this should also have lock
+    sem_t commandIndicator;            // initial value is 0. it's purpose is
+                                       // to indicate about new command
+    int commandType;                   // indicated commandType
 
     //The camData part
     sem_t produced; // 0-10
     sem_t consumed; // 0-10
 
-    sem_t cIndexLock;           // 0-1 to access consumedUpTo
-    int consumedUpTo;           // index of consumedUpTo (cumulative)
-    sem_t pIndexLock;           // 0-1 to access producedUpTo
-    int producedUpTo;           // index of producedUpTo (cumulative)
-    struct CamData camdata[10]; // the data read from cameras will end up in this array
-                                // each camdata have 60 frames of particular camera
+    sem_t cIndexLock; // 0-1 to access consumedUpTo
+    int consumedUpTo; // index of consumedUpTo (cumulative)
+    sem_t pIndexLock; // 0-1 to access producedUpTo
+    int producedUpTo; // index of producedUpTo (cumulative)
+    struct CamData
+        camdata[MAX_CAM_NUMBER * 2]; // the data read from cameras will end up in this array
+                                     // each camdata have 60 frames of particular camera
 };
 
 #endif
