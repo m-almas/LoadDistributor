abcfa4fd00f1406e786dfb87134b506b83c6f310
Pupye <fullhouseya89@gmail.com>
2020-03-11 15:06:20 +0600

    introducing structure in simpleGpuManager

--- simpleGpuManager.c
+++ simpleGpuManager.c
@@ -13,59 +13,21 @@
 
 #include "dataBlock.h"
 
-#define BUSY 7
-#define EMPTY 5
-#define FILLED 3
+void *getSharedMem(key_t ShmKEY);
 
-int getFilledIndex(int *index, int length)
-{
-    for (size_t i = 0; i < length; i++)
-    {
-        if (index[i] == FILLED)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
+int getFilledIndex(int *index, int length);
 
-void consumeCamData(struct CamData *camdata)
-{
-    printf("----------------------------\n");
-    for (size_t i = 0; i < 10; i++)
-    {
-        for (size_t j = 0; j < 5; j++)
-        {
-            printf(" %i ", camdata->frames[i].frameData[j]);
-            //here we should write it into file
-        }
-        printf("\n");
-    }
-    printf("----------------------------\n");
-}
+void consumeCamData(struct CamData *camdata);
 
 int main(int argc, char *argv[])
 {
     key_t ShmKEY;
     int ShmID;
     pid_t pid;
-    struct DataBlock *ShmPTR;
-    /*to get unigue identifier*/
-    ShmKEY = ftok("..", 'x');
-    //allocating shared block
-    ShmID = shmget(ShmKEY, sizeof(struct DataBlock), 0666);
-    if (ShmID < 0)
-    {
-        printf("*** shmget error ***\n");
-        exit(1);
-    }
-
-    ShmPTR = (struct DataBlock *)shmat(ShmID, NULL, 0);
-    if ((int)ShmPTR == -1)
-    {
-        printf("*** shmat error ***\n");
-        exit(1);
-    }
+    struct DataBlock *ShmDataBlock;
+    /*to get unigue identifier for sharedMemory*/
+    ShmKEY = ftok(".", 23);
+    ShmDataBlock = (struct DataBlock *)getSharedMem(ShmKEY);
 
     for (size_t j = 0; j < 1; j++)
     {
@@ -83,19 +45,19 @@ int main(int argc, char *argv[])
         {
             //something produced
 
-            sem_wait(&ShmPTR->produced);
-            sem_wait(&ShmPTR->cIndexLock);
+            sem_wait(&ShmDataBlock->produced);
+            sem_wait(&ShmDataBlock->cIndexLock);
             //get consumed index and mode it with 10
-            consumedIndex = ShmPTR->consumedUpTo;
-            ShmPTR->consumedUpTo = (consumedIndex + 1) % 10;
-            sem_post(&ShmPTR->cIndexLock);
+            consumedIndex = ShmDataBlock->consumedUpTo;
+            ShmDataBlock->consumedUpTo = (consumedIndex + 1) % 10;
+            sem_post(&ShmDataBlock->cIndexLock);
 
             //here we get the data and do our stuff
             sleep(1);
-            consumeCamData(&(ShmPTR->camdata[consumedIndex]));
+            consumeCamData(&(ShmDataBlock->camdata[consumedIndex]));
             fflush(stdout);
 
-            sem_post(&ShmPTR->consumed);
+            sem_post(&ShmDataBlock->consumed);
         }
     }
     else
@@ -108,4 +70,40 @@ int main(int argc, char *argv[])
             }
         }
     }
+}
+
+void *getSharedMem(key_t ShmKEY)
+{
+    int ShmID;
+    struct DataBlock *ShmPTR;
+
+    ShmID = shmget(ShmKEY, sizeof(struct DataBlock), 0666);
+    if (ShmID < 0)
+    {
+        printf("*** shmget error ***\n");
+        exit(1);
+    }
+
+    ShmPTR = (struct DataBlock *)shmat(ShmID, NULL, 0);
+    if ((int)ShmPTR == -1)
+    {
+        printf("*** shmat error ***\n");
+        exit(1);
+    }
+    return ShmPTR;
+}
+
+void consumeCamData(struct CamData *camdata)
+{
+    printf("----------------------------\n");
+    for (size_t i = 0; i < 10; i++)
+    {
+        for (size_t j = 0; j < 5; j++)
+        {
+            printf(" %i ", camdata->frames[i].frameData[j]);
+            //here we should write it into file
+        }
+        printf("\n");
+    }
+    printf("----------------------------\n");
 }
